b9390fd06c49fdfeafc5f7ddce4bfb443e658eb1
nik <niklas@nbuchberger.de>
2025-10-24 10:24:35 +0200

    comments + cleaner code

--- SportActivityManager.py
+++ SportActivityManager.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import matplotlib
-import matplotlib as mpl
 from matplotlib.colors import ListedColormap
 
 class SportActivityManager(object):
@@ -22,22 +21,10 @@ class SportActivityManager(object):
                 file.close() 
                 self.activity_data = dict()
         self.text_to_value_mapping = {
-            "restday" : 
-                {
-                    "numeric_value" : 1,
-                },
-            "workout" : 
-                {
-                    "numeric_value" : 2,
-                },
-            "running" :
-                {
-                    "numeric_value" : 3,
-                },
-            "hiking" :
-                {
-                    "numeric_value" : 4,
-                } 
+            "restday" : 1,
+            "workout" : 2,
+            "running" : 3,
+            "hiking" : 4
         }
         self.num_to_display_mapping = {
             "1" : "\U0001F4A4",
@@ -45,60 +32,54 @@ class SportActivityManager(object):
             "3" : "\U0001F3C3",
             "4" : "\U0001F97E"
         }
-        
 
-    def create_entry(self,default_date=""):
+    def create_entry(self,default_date:str=""):
         '''
-        Create a new entry over console input and safe it into the json file
+        Create a new entry over console input and save it into the json file.
+        Parameters:
+            - default_date: str (used when generating multiple entries)
         '''
-        #print("Enter a date for the activity (leave empty if its for today)")
-        #print("Enter activity type (workout (w), running (l) or restday (r))")
-        #print("Enter any additional details (distance, workout name, etc.)\n")
         if default_date == "":
-            date = input("Enter date (yyyy-mm-dd): ")
+            date = input("Enter date (yyyy-mm-dd, leave empty for todays date): ")
             if len(date) == 0:
                 date = default_date
         else:
             date = default_date
             print(f"Date: {default_date}")
-        type = input("Enter type: ")
+        type = input("Enter type (workout w, running r, hiking h or restday ENTER): ")
         if len(type) == 1:
-            type = {"w" : "workout", "l": "running", "r": "restday", "h": "hiking"}[type]
-        details = input("Enter additional details: ")
+            type = {"w" : "workout", "r": "running", "h": "hiking"}[type]
+        else:
+            type = "restday"
+        details = input("Enter additional details (distance, workout name, etc.)")
         entry = {
             date: {
                 "activity_type" : type,
                 "activity_details" : details
             }
         }
-        '''
-        This is how one JSON entry should look like
-        {
-            "2025-10-24": {
-                "activity_type" : "workout, running, restday",
-                "activity_details" : "km / workout name"
-            }
-        }
-        '''
         self.activity_data.update(entry)
-        self.save_activity_date(entry)
-
-    def save_activity_date(self,entry):
         with open("sport_activity_data.json", "w") as file:
             json.dump(self.activity_data,file,indent=4)
 
-    def create_heatmap(self,start_date,end_date):
+    def create_activity_map(self,start_date:str,end_date:str):
+        '''
+        Creates an activity map for all the activity from start to end date.
+        '''
+        # Default dates (min or max date)
         if start_date == "":
             start_date = list(self.activity_data.items())[0][0]
         if end_date == "":
             end_date = list(self.activity_data.items())[-1][0]
-        date_range = pandas.date_range(start=start_date,end=end_date)
 
+        # Create Date Range and initial data structures
+        date_range = pandas.date_range(start=start_date,end=end_date)
         calendar_weeks = list()
         weekdays = [1,2,3,4,5,6,7]
-        data = []
+        data = list()
         row_data = [0,0,0,0,0,0,0]
 
+        # Generating the data for the map as a list of row based lists
         for date in date_range:
             date = str(date)[:10]
             datetime_object = datetime.datetime.strptime(date, '%Y-%m-%d').date()
@@ -106,41 +87,42 @@ class SportActivityManager(object):
             if calendar_week not in calendar_weeks:
                 calendar_weeks.append(calendar_week)
             try: 
-                val = self.text_to_value_mapping[self.activity_data[date]["activity_type"]]["numeric_value"] 
+                val = self.text_to_value_mapping[self.activity_data[date]["activity_type"]] 
             except:
                 val = 0
-            weekday = datetime_object.isocalendar().weekday # 7 is sunday!
+            weekday = datetime_object.isocalendar().weekday
             row_data[weekday-1] = val
             if weekday == 7 or date == end_date:
                 data.append(row_data)
                 row_data = [0,0,0,0,0,0,0]
-            
+                
+        structured_data = np.array(data)
 
-        test = np.array(data)
-
+        # Basic plot config
         colors = ["#F9DBBD", "#FF858D", "#DA627D", "#A53860", "#450920"]
-        cmap = ListedColormap(colors)
-
+        background_color = "#F1BB7E"
+        color_map = ListedColormap(colors)
         fig, ax = plt.subplots()
-        im = ax.imshow(test,cmap)
-        fig.patch.set_facecolor("#F1BB7E")
-
-        # Set calendar week and weekday ticks
+        plt.rc('font', family='Consolas', size=12)
+        im = ax.imshow(structured_data,cmap=color_map)
+        fig.patch.set_facecolor(background_color)
         ax.set_xticks(range(len(weekdays)), labels=weekdays)
         plt.xlabel("Weekdays")
         ax.set_yticks(range(len(calendar_weeks)), labels=calendar_weeks)
         plt.ylabel("Calendar week")
 
+        # Adding text on top of the heatmap squares
         for i in range(len(calendar_weeks)):
             for j in range(len(weekdays)):
                 try:
-                    text = self.num_to_display_mapping[str(test[i,j])]
+                    text = self.num_to_display_mapping[str(structured_data[i,j])]
                 except:
                     text = ""
                 text = ax.text(j, i, text, ha="center", va="center", color="w", fontfamily="Segoe UI Emoji", fontsize=20)
 
         ax.set_title(f"Sport Activity Data from {start_date} to {end_date}")
 
+        # Legend for the plot
         legend_labels = {
             0: "No data",
             1: "Restday",
@@ -148,28 +130,23 @@ class SportActivityManager(object):
             3: "Running",
             4: "Hiking"
         }
-
         patches = [matplotlib.patches.Patch(color=colors[i], label=legend_labels[i]) for i in range(len(colors))]
-
-        # Legende hinzufügen
         ax.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc='upper left', title="Activity Type")
 
         plt.show()
-
-        
 
 if __name__ == "__main__":
     s = SportActivityManager()
     user_input = ""
     while user_input == "":
-        user_input = input("Generate activiy map (M) or enter data (D)? ")
+        user_input = input("Generate activiy map (M), enter new entry (D) or enter mode to add multiple entries (X)? ")
         if user_input == "D":
             s.create_entry()
         elif user_input == "M":
             print("Please specify the start and end date for the activity map!")
             start = input("Start date (yyyy-mm-dd): ")
             end = input("End date (yyyy-mm-dd): ")
-            s.create_heatmap(start,end)
+            s.create_activity_map(start,end)
         elif user_input == "X":
             print("Special entry mode")
             start = input("Start date (yyyy-mm-dd): ")
@@ -179,25 +156,5 @@ if __name__ == "__main__":
                 date = str(date)[:10]
                 s.create_entry(date)
         else:
+            print("Bye")
             break
-
-'''
-{
-    "2025-10-24": {
-        "activity_type": "workout",
-        "activity_details": "2"
-    },
-    "2025-10-25": {
-        "activity_type": "workout",
-        "activity_details": "3"
-    },
-    "2025-10-26": {
-        "activity_type": "restday",
-        "activity_details": "1"
-    },
-    "2025-10-27": {
-        "activity_type": "running",
-        "activity_details": "1"
-    }
-}
-'''
